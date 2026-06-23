5206f80015cbdf90b073f63938853a03cb0e67bb
Kanishck Tewatia <tewatia2k@gmail.com>
2026-06-24 03:52:58 +0530

    Initial commit

---
+++ Operators.py
@@ -0,0 +1,51 @@
+
+# Arithmatic Operators
+
+a = 10
+b= 20
+
+#addition
+c=a+b
+print(c)
+
+#sub
+d=a-b
+print(d)
+
+# multiply
+e = a*b
+print(e)
+
+#div
+f=a/b
+print(f)
+
+#Floor Div
+x=15
+y=10
+print(x/y)
+print(x//y)
+
+#Modulo Operator
+print(x%y)
+
+#Exponential
+a=2
+b=3
+print(a**b)
+
+#Comparison 
+#these operators return boolean value (True/False)
+
+#Greater than or equal to
+
+# Not equals to
+
+a=1
+b=5
+print(a!=b)
+
+a=11
+print((10<a) and (a<20))
+
+print(a<50 Or a>13)
